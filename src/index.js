4ca5e643e5c00e1725a6bd5fda12bbf590e93431
ARTUR-SENAI <0000880096@senaimgaluno.com.br>
2022-08-12 19:59:25 -0300

    siim

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 //backend
+import { createServer } from 'https';
+import { readFileSync } from 'fs';
+import { WebSocketServer } from 'ws';
 
-import { Socket } from 'dgram';
+const server = createServer({
+  cert: readFileSync('/path/to/cert.pem'),
+  key: readFileSync('/path/to/key.pem')
+});
+const wss = new WebSocketServer({ server });
 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
 
-import express from 'express';
-import http from 'http';
-import socketio from 'socket.io'
+  ws.send('something');
+});
 
-const app = express();
-const server = http.Server(app);
-app.use(express.static(__dirname + '/public')) // responsavel por guardar uma pasta, para q consiga usar a pasta depois
+server.listen(8080);
+ // responsavel por guardar uma pasta, para q consiga usar a pasta depois
 
 const io = socketio(server); //conexao com do socket com servidor
 
